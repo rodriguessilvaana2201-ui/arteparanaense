9323939ac5f016b01ee2b4568b469669cc685369
rodriguessilvaana2201-ui <rodrigues.silva.ana2201@escola.pr.gov.br>
2026-09-09 17:37:04 -0300

    atualizei meu codigo

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-
+const botao = document. querySelector("#botaomensagem");
+cons mensagem = document. querySelector("#mensageminterativa");
+botao. addEvetListener("clik", mostrarmesagem);
+function mostrarMensagem() {
+    Mensagem.textContent =
+    "vocẽ acabou de criar uma interaçãocom javaScript";
+}
